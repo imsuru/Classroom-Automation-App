84dd8018b2ab344c19bae19a63232b2fed021e37
suraj nayak <sn013293@gmail.com>
2022-10-07 10:34:09 +0530

    Android Manifest xml file

---
+++ app/src/main/java/com/example/classroomautomation/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.classroomautomation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import anroid
+        @Preview(showBackround = true)
+    @Composable
+fun DefaultPreview(){
+    FirstComposeProjectTheme{
+        Greeting("Anroid")
+        }
+}
