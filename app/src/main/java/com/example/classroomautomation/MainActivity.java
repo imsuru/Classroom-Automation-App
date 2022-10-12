1c73458490dbc0999e90a23ccf3d2720caeb8cc8
suraj nayak <suraj.nayak@student.vishwaniketan.edu.in>
2022-10-12 09:32:58 +0530

    Android Manifest xml file

--- app/src/main/java/com/example/classroomautomation/MainActivity.java
+++ app/src/main/java/com/example/classroomautomation/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.classroomautomation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import anroid
-        @Preview(showBackround = true)
-    @Composable
-fun DefaultPreview(){
-    FirstComposeProjectTheme{
-        Greeting("Anroid")
-        }
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
