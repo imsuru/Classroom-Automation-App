0df616e404e482705b37fcada82e680a67efa261
suraj nayak <suraj.nayak@student.vishwaniketan.edu.in>
2022-10-31 23:36:42 +0530

    Android Manifest xml file

--- app/src/main/java/com/example/classroomautomation/MainActivity.java
+++ app/src/main/java/com/example/classroomautomation/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.classroomautomation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button DBMS, button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DBMS = (Button) findViewById(R.id.button);
+        DBMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main2);
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, button);
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        // Toast message on menu item clicked
+                        Toast.makeText(MainActivity.this, "You Clicked " + menuItem.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+
+
+                       // setContentView(R.layout.activity_main2);
+
+
+
+                    }
+
+
+                });
+                // Showing the popup menu
+                popupMenu.show();
+            }
+
+
+        });
     }
 }
--- app/src/main/java/com/example/classroomautomation/MainActivity2.java
+++ app/src/main/java/com/example/classroomautomation/MainActivity2.java
@@ -2,13 +2,47 @@ package com.example.classroomautomation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    Switch fan1;
+    Switch fan2;
+    Switch fan3;
+    Switch fan4;
+    Switch Light1;
+    Switch Light2;
+    Switch Light3;
+    Switch Light4;
+    Switch Light5;
+    Switch Light6;
+    Switch Light7;
+    Switch Light8;
+
+
+
+    protected void onCreate(Bundle savedInstanceState, Object view) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        // initiate view's
+
+        fan1 = findViewById(R.id.fan1);
+        fan2 = findViewById(R.id.fan2);
+        fan3 = findViewById(R.id.fan3);
+        fan4 = findViewById(R.id.fan4);
+
+        Light1 = findViewById(R.id.Light1);
+        Light2 = findViewById(R.id.Light2);
+        Light3 = findViewById(R.id.Light3);
+        Light4 = findViewById(R.id.Light4);
+        Light5 = findViewById(R.id.Light5);
+        Light6 = findViewById(R.id.Light6);
+        Light7 = findViewById(R.id.Light7);
+        Light8 = findViewById(R.id.Light8);
+
+
     }
 }
